1e108839a1b7e93a9b09398625b1d88051547362
AGISYA <abdiagisya@gmail.com>
2025-01-24 10:07:05 +0700

    icon menu mobile

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -31,97 +31,68 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`bg-[#1b3b34] text-white px-6 py-2 fixed w-full z-50 transition-transform duration-200 ${
+      className={`bg-[#1b3b34] text-white px-6 py-3 fixed w-full z-50 shadow-md transition-transform duration-300 ${
         showNavbar ? "translate-y-0" : "-translate-y-full"
       }`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo Teks */}
-        <Link href="/" className="hover:opacity-80 transition duration-200">
-          <img src="/images/logo1.png" alt="Logo" className="h-16" />
-        </Link>
-        {/* Menu Tengah */}
-        <ul className="hidden md:flex space-x-6">
-          <li>
-            <Link
-              href="#home"
-              className="hover:text-gray-400 transition duration-200"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#aboutus"
-              className="hover:text-gray-400 transition duration-200"
-            >
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#menu"
-              className="hover:text-gray-400 transition duration-200"
-            >
-              Menu
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#contact"
-              className="hover:text-gray-400 transition duration-200"
-            >
-              Contact Us
-            </Link>
-          </li>
-        </ul>
+        {" "}
         {/* Hamburger Menu */}
         <div className="md:hidden">
           <button
             onClick={toggleMenu}
-            className="text-white hover:text-gray-400"
+            className="text-white hover:text-gray-400 focus:outline-none"
           >
             {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
+        {/* Logo */}
+        <Link href="/" className="hover:opacity-80 transition duration-200">
+          <img src="/images/logo1.png" alt="Logo" className="h-16" />
+        </Link>
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6">
+          {["Home", "About Us", "Menu", "Contact Us"].map((item, idx) => (
+            <li key={idx}>
+              <Link
+                href={`#${item.toLowerCase().replace(" ", "")}`}
+                className="hover:text-gray-400 transition duration-200"
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
 
-      {isMenuOpen && (
-        <ul className="md:hidden mt-4 space-y-4 text-center bg-[#1b3b34] py-4 border-t border-gray-800">
-          <li>
-            <Link
-              href="#home"
-              className="block hover:text-gray-400 transition duration-200"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#aboutus"
-              className="block hover:text-gray-400 transition duration-200"
-            >
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#menu"
-              className="block hover:text-gray-400 transition duration-200"
-            >
-              Menu
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#contact"
-              className="block hover:text-gray-400 transition duration-200"
-            >
-              Contact Us
-            </Link>
-          </li>
+      {/* Mobile Menu */}
+      <div
+        className={`md:hidden bg-[#1b3b34] fixed top-0 left-0 w-full h-screen z-40 transform transition-transform duration-300 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="p-6">
+          <button
+            onClick={toggleMenu}
+            className="text-white hover:text-gray-400 focus:outline-none"
+          >
+            <FaTimes size={24} />
+          </button>
+        </div>
+        <ul className="space-y-6 text-center mt-16">
+          {["Home", "About Us", "Menu", "Contact Us"].map((item, idx) => (
+            <li key={idx}>
+              <Link
+                href={`#${item.toLowerCase().replace(" ", "")}`}
+                className="block text-lg text-white hover:text-gray-400 transition duration-200"
+                onClick={toggleMenu}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
         </ul>
-      )}
+      </div>
     </nav>
   );
 };
